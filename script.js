4892a527c3ffe0c108c84f683fb704e2c42472c6
XcMore <104893099+XcMore-pop@users.noreply.github.com>
2026-03-03 21:41:51 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
+const API_URL = "https://docs.google.com/spreadsheets/d/15IaYOCESVL4rehHOPTi8DGB8nvK5PUa2-dkbn_ZmzEg/edit?usp=sharing";
 
+fetch(API_URL)
+.then(res => res.json())
+.then(data=>{
+  let container = document.getElementById("portfolio");
+  data.forEach(row=>{
+    let card = document.createElement("div");
+    card.className = "card";
+    card.innerHTML = `
+      <h3>${row[1]}</h3>
+      <p>${row[3]}</p>
+    `;
+    container.appendChild(card);
+  });
+});
+
+window.addEventListener("scroll", ()=>{
+  document.querySelectorAll(".card").forEach(card=>{
+    let top = card.getBoundingClientRect().top;
+    if(top < window.innerHeight - 100){
+      card.style.opacity = 1;
+      card.style.transform = "translateY(0)";
+    }
+  });
+});
